aa45fbc29748f2e679b64428bc1495e6947a564b
cindy <sieun6213@gmail.com>
2024-09-12 15:28:43 +0900

    Time-layout

--- app/src/main/java/com/example/knockknock/view/MainActivity.java
+++ app/src/main/java/com/example/knockknock/view/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.knockknock.view;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,21 +14,57 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.knockknock.R;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView ShowDate;
+    private TextView ShowTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
 
-        });
-        */
+        ShowDate = (TextView) findViewById(R.id.textView4);
+        ShowTime = (TextView) findViewById(R.id.textView5);
+
+
+        ShowTimeMethod();
+    }
+
+    public void ShowTimeMethod(){
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+//                ShowTime.setText(DateFormat.getDateTimeInstance().format(new Date()));
+                Calendar cal = Calendar.getInstance();
+
+                SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd(E)");
+                String strDate = date.format(cal.getTime());
+                SimpleDateFormat time = new SimpleDateFormat("hh:mm a");
+                String strTime = time.format(cal.getTime());
+                ShowDate.setText(strDate);
+                ShowTime.setText(strTime);
+            }
+        };
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){}
+                    handler.sendEmptyMessage(1);
+                }
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
     }
 } //test
 //test2
